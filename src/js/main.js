d3ef79b47f654613d7b84de443c4c9323ab76bc7
Adrian Biedrzynski <biedrzynski.adrian@gmail.com>
2019-08-05 13:56:25 +0200

    feat. js complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,17 @@
-"use strict";
-
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
 // place your code below
 
+const textArea = document.querySelector('.text');
+const buttonSave = document.querySelector('.notepad__save');
+const buttonLoad = document.querySelector('.notepad__load');
 
-console.log(`Hello world!`);
+buttonSave.addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem('textArea', textArea.value);
+})
+
+buttonLoad.addEventListener('click', (e) => {
+  e.preventDefault();
+  textArea.value = localStorage.getItem('textArea');
+})
 
 
